06f3d1d952de08a4ee2c2a2aaaa399ef8173d1fb
ezzygarmyz <dragkhai@gmail.com>
2026-01-04 00:14:43 +0100

    bump to version 1.2.0

--- insightwallet/__main__.py
+++ insightwallet/__main__.py
@@ -56,7 +56,7 @@ def main():
     app = InsightWallet(
         formal_name="InsightWallet",
         app_id="com.btcz.insightwallet",
-        version="1.1.0",
+        version="1.2.0",
         icon="resources/insightwallet"
     )
     app.main_loop()
